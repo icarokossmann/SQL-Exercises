c9612917763e026102bc5e0e677acd3f6d9fe4bc
Icaro Kossmann <icaro.kossmann@gmail.com>
2023-06-07 23:19:38 -0300

    Upload novos arquivos

--- Script 5 - Freecodecamp.sql
+++ Script 5 - Freecodecamp.sql
@@ -1,1 +1,16 @@
-CREATE TABLE
+-- FUNCTIONS
+-- Find the number of Employees
+SELECT COUNT(emp_id)
+FROM employee;
+
+SELECT COUNT(super_id)
+FROM employee;
+
+-- Find the number of female employees born after 1970
+SELECT COUNT(emp_id)
+FROM employee
+WHERE sex = 'F' AND birth_date > '1971-01-01';
+
+-- Find the average of all employee´s salaries
+SELECT AVG(salary)
+FROM employee;
